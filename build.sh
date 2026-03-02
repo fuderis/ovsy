240978ad2a85b4fa9d90d889eff9673a58b8176e
Bulat Sh. (fuderis) <synapdrake@ya.ru>
2026-03-03 02:17:33 +0500

    Updated build script

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@ set -e
 
 echo "Building Ovsy core.."
 cargo build --release
+cd agents
+
+echo "Building Power agent.."
+cd power && cargo build --release
 
 echo "Building Music agent.."
-cd agents/music && cargo build --release
+cd ../music && cargo build --release
+
+cd ../../
 
 echo "Installing to /opt/ovsy/"
-cd ../..
 sudo mkdir -p /opt/ovsy
 sudo chown $USER:$USER /opt/ovsy
 
@@ -15,10 +20,14 @@ sudo chown $USER:$USER /opt/ovsy
 cp target/release/ovsy /opt/ovsy/
 chmod 755 /opt/ovsy/ovsy
 
-# music:
-mkdir -p /opt/ovsy/agents/music/target/release
-cp agents/music/Ovsy.toml /opt/ovsy/agents/music/
-cp agents/music/target/release/music /opt/ovsy/agents/music/target/release/
-chmod 755 /opt/ovsy/agents/music/target/release/music
+install_agent() {
+  mkdir -p "/opt/ovsy/agents/$1/target/release"
+  cp "agents/$1/Ovsy.toml" "/opt/ovsy/agents/$1/"
+  cp "agents/$1/target/release/$1" "/opt/ovsy/agents/$1/target/release/"
+  chmod 755 "/opt/ovsy/agents/$1/target/release/$1"
+}
+
+install_agent music
+install_agent power
 
 echo "✅ Installed!"
